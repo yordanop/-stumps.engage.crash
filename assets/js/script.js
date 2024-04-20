abab3653759627f424d80bbe88879f8638f2663f
Eynar <yordanopv@gmail.com>
2024-04-19 19:50:22 -0600

    function to generate unique id

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,13 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    let newTaskID;
+    if (taskList){
+         newTaskID =  (taskList.id + 1 + 0xFFFFF).toString(16);
+    }else{
+        newTaskID = (1 + 0xFFFFF).toString(16);
+    }
+    return newTaskID;
 }
 
 // Todo: create a function to create a task card
@@ -34,5 +40,5 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    generateTaskId();
 });
