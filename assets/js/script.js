d936aa401096549aa1a4c91261108ddf7b6b989e
Eynar <yordanopv@gmail.com>
2024-04-20 22:06:52 -0600

    added datepicker widget

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,14 +17,14 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
-    const taskCard = $(`<div class="card text-center task-card" data-taskid="#${task.taskID}">
+    const taskCard = $(`<div class="card text-center bg-danger task-card draggable" data-taskid="#${task.taskID}">
                             <div class="card-header">
                                 ${task.title}
                             </div>
                             <div class="card-body">
                                 <h5 class="card-title">${task.description}</h5>
                                 <p class="card-text">${task.date}</p>
-                                <button class="btn btn-primary delete-task-btn">Delete</a>
+                                <button class="btn btn-danger btn-success-border-subtle delete-task-btn">Delete</a>
                             </div>
                             </div>`);
 
@@ -90,6 +90,7 @@ function handleDeleteTask(event){
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
+
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -102,11 +103,14 @@ $(document).ready(function () {
         renderTaskList();
         toDoSection.on('click', '.delete-task-btn', handleDeleteTask);
     }
-    
-    
 
+    $(function () {
+        $('#date-task').datepicker({
+          changeMonth: true,
+          changeYear: true,
+        });
+      });
+    
     addTaskButton.on('click', handleAddTask);
 
-    
-   
 });
